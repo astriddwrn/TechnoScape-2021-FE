ab9f4f400e780d01e86ef07de4453e39f26b8d12
Alfian <alfian101@gmail.com>
2021-03-27 11:44:15 +0700

    mentors non responsive done

--- js/pages/hackathon.js
+++ js/pages/hackathon.js
@@ -15,3 +15,44 @@ $(window).scroll(() => {
 	}
 	prevPosition = currPosition;
 });
+
+/* Mentors */
+/* mentorsButton */
+$(document).ready(function(){
+    $("#mentors-design-button").click(function () {
+		$("#mentors-design-button").addClass("mentors-buttons-active");
+		$("#mentors-technology-button").removeClass("mentors-buttons-active");
+		$("#mentors-business-button").removeClass("mentors-buttons-active");
+	});
+	$("#mentors-technology-button").click(function () {
+		$("#mentors-technology-button").addClass("mentors-buttons-active");
+		$("#mentors-design-button").removeClass("mentors-buttons-active");
+		$("#mentors-business-button").removeClass("mentors-buttons-active");
+	});
+	$("#mentors-business-button").click(function () {
+		$("#mentors-business-button").addClass("mentors-buttons-active");
+		$("#mentors-technology-button").removeClass("mentors-buttons-active");
+		$("#mentors-design-button").removeClass("mentors-buttons-active");
+    });
+});
+
+/* mentorsCardShow */
+$(document).ready(function(){
+	$("#mentors-design-button").click(function () {
+		$("#mentors-design").show();
+		$("#mentors-technology").hide();
+	 	$("#mentors-business").hide();
+	});
+	$("#mentors-technology-button").click(function () {
+		$("#mentors-technology").show();
+		$("#mentors-design").hide();
+	 	$("#mentors-business").hide();
+	});
+	$("#mentors-business-button").click(function () {
+		$("#mentors-business").show();
+		$("#mentors-technology").hide();
+	 	$("#mentors-design").hide();
+	});
+});
+  
+/* Mentors Carousel */
