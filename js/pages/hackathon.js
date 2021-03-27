839b7115e06456b333923182760d2c5b134fa459
astriddwrn <astrid.dewi.rana@gmail.com>
2021-03-27 15:08:10 +0700

    hackathon champion carousel fading on prog

--- js/pages/hackathon.js
+++ js/pages/hackathon.js
@@ -1,88 +1,174 @@
-let prevPosition = $(window).scrollTop();
-$(window).scroll(() => {
-	let currPosition = $(window).scrollTop();
-	if (prevPosition > currPosition) {
-		$("#navbar").removeClass("scroll-down");
-		$("#navbar").addClass("scroll-up");
-	} else {
-		$("#navbar").removeClass("scroll-up");
-		$("#navbar").addClass("scroll-down");
-	}
-	if (currPosition > 700) {
-		$("#navbar").addClass("change-bg");
-	} else {
-		$("#navbar").removeClass("change-bg");
-	}
-	prevPosition = currPosition;
-});
+// let prevPosition = $(window).scrollTop();
+// $(window).scroll(() => {
+// 	let currPosition = $(window).scrollTop();
+// 	if (prevPosition > currPosition) {
+// 		$("#navbar").removeClass("scroll-down");
+// 		$("#navbar").addClass("scroll-up");
+// 	} else {
+// 		$("#navbar").removeClass("scroll-up");
+// 		$("#navbar").addClass("scroll-down");
+// 	}
+// 	if (currPosition > 700) {
+// 		$("#navbar").addClass("change-bg");
+// 	} else {
+// 		$("#navbar").removeClass("change-bg");
+// 	}
+// 	prevPosition = currPosition;
+// });
 
 
-// Why You Should Join
+// // Why You Should Join
+// $(document).ready(function(){
+// 	$('.carousel-wrapper').slick({
+// 		centerMode: true,
+// 		focusOnSelect: true,
+// 		slidesToShow: 3,
+// 		dots: true,
+//   		arrows: true,
+//   		swipe: true,
+// 		infinite: true,
+// 		pauseOnFocus: false,
+//         pauseOnHover: false,
+// 		prevArrow: $('.prev-container'),
+// 		nextArrow: $('.next-container')
+// 	});
+
+
+
+// 	function setSlideVisibility() {
+// 		var invisibleSlides = $('.slick-slide[aria-hidden="true"]');
+// 		var visibleSlides = $('.slick-slide[aria-hidden="false"]');
+// 		$(visibleSlides).each(function() {
+// 		  $(this).animate({
+// 			opacity: 1 ,
+// 		}, 400);
+// 		});
+// 		$(visibleSlides).first().prev().animate({
+// 			opacity: 0,
+// 		}, 400);
+// 		$(invisibleSlides).css('opacity', 0);
+// 	  }
+
+// 	  $(setSlideVisibility());
+	  
+// 	  $('.carousel-wrapper').on('afterChange', function() {
+// 		setSlideVisibility();
+// 	  });
+
+
+//   });
+
+
 $(document).ready(function(){
-	$('.carousel-wrapper').slick({
-		centerMode: true,
-		focusOnSelect: true,
-		slidesToShow: 3,
-		dots: true,
-  		arrows: true,
-  		swipe: true,
-  		// swipeToSlide: false,
-		infinite: true,
-		pauseOnFocus: false,
-        pauseOnHover: false,
-		prevArrow: $('.prev-container'),
-		nextArrow: $('.next-container')
+	$(".join .glass").each(function(){
+		$(this).addClass("invisibleJoin");
 	});
 
-	// function removeLeftSlide(){
-	// 	var center = $('.slick-center');
-	// 	var centerIndx = $('.slick-center[slick-index]' - 1);
+	var indexMiddle=0;
+	var indexLeft=3;
+	var indexRight=1;
 
-	// 	$(centerIndx).hide();
-	// };
+	$(".join .glass").eq(indexMiddle).removeClass("invisibleJoin").addClass("activeJoin");
+	$(".join .glass").eq(indexLeft).removeClass("invisibleJoin").addClass("leftJoin");
+	$(".join .glass").eq(indexRight).removeClass("invisibleJoin").addClass("rightJoin");
 
-	// $('next-container').click(function(){
-	// 	$(removeLeftSlide());
-	// });
+	$(".join .glass").each(function(){
+		$(this).animate({
+		opacity: 1,
+		}, 400)
+	});
 
-
-
-	function setSlideVisibility() {
-		var invisibleSlides = $('.slick-slide[aria-hidden="true"]');
-		//Find the visible slides i.e. where aria-hidden="false"
-		var visibleSlides = $('.slick-slide[aria-hidden="false"]');
-		//Make sure all of the visible slides have an opacity of 1
-		$(visibleSlides).each(function() {
-		  $(this).animate({
-			opacity: 1 ,
-		}, 400);
-		});
-		//Set the opacity of the first and last partial slides.
-		$(visibleSlides).first().prev().animate({
+	// next
+	$(".join .next").click(function(){
+		$(".join .glass").each(function(){
+			$(this).animate({
 			opacity: 0,
-		}, 400);
-		// $(visibleSlides).last().next().animate({
-		// 	opacity: 0,
-		// }, 200);
+			}, 400)
+		});
 
-		$(invisibleSlides).css('opacity', 0);
-	  }
-	  
-	  
+		$(".join .glass").eq(indexMiddle).removeClass("activeJoin");
+		$(".join .glass").eq(indexLeft).removeClass("leftJoin");
+		$(".join .glass").eq(indexRight).removeClass("rightJoin");
 
-	  $(setSlideVisibility());
-	  
-	//   $('.carousel-wrapper').on('beforeChange', function() {
-	// 	$('.slick-slide').each(function() {
-			
-	// 		$(this).css('opacity', 1);
-	// 		$(invisibleSlides).removeClass('invisible');
-	// 	});
-	//   });
-	  
-	  $('.carousel-wrapper').on('afterChange', function() {
-		setSlideVisibility();
-	  });
+		$(".join .glass").each(function(){
+			$(this).addClass("invisibleJoin");
+		});
+
+		indexMiddle+=1;
+		indexLeft+=1;
+		indexRight+=1;
+
+		if(indexMiddle>3){
+			indexMiddle=0;
+		}
+		if(indexLeft>3){
+			indexLeft=0;
+		}
+		if(indexRight>3){
+			indexRight=0;
+		}
 
 
-  });
+		$(".join .glass").eq(indexMiddle).removeClass("invisibleJoin").addClass("activeJoin");
+		$(".join .glass").eq(indexLeft).removeClass("invisibleJoin").addClass("leftJoin");
+		$(".join .glass").eq(indexRight).removeClass("invisibleJoin").addClass("rightJoin");
+
+		$(".join .glass").each(function(){
+			$(this).animate({
+			opacity: 1,
+			}, 400)
+		});
+
+
+	});
+
+
+	// prev
+	$(".join .prev").click(function(){
+		$(".join .glass").eq(indexMiddle).removeClass("activeJoin");
+		$(".join .glass").eq(indexLeft).removeClass("leftJoin");
+		$(".join .glass").eq(indexRight).removeClass("rightJoin");
+
+		$(".join .glass").each(function(){
+			$(this).addClass("invisibleJoin");
+		});
+
+		indexMiddle-=1;
+		indexLeft-=1;
+		indexRight-=1;
+
+		if(indexMiddle<0){
+			indexMiddle=3;
+		}
+		if(indexLeft<0){
+			indexLeft=3;
+		}
+		if(indexRight<0){
+			indexRight=3;
+		}
+
+		$(".join .glass").eq(indexMiddle).removeClass("invisibleJoin").addClass("activeJoin");
+		$(".join .glass").eq(indexLeft).removeClass("invisibleJoin").addClass("leftJoin");
+		$(".join .glass").eq(indexRight).removeClass("invisibleJoin").addClass("rightJoin");
+
+
+	});
+
+
+	// $(".join .glass").eq(1).removeClass("invisibleJoin").addClass("activeJoin")
+	// var indexSlide=$(".join .glass").index();
+	
+	// if(indexSlide==1){
+	// 	$(this).removeClass("invisibleJoin");
+	// }
+	// function activee(){
+	// 	$(this).removeClass("invisibleJoin");
+	// 	$(this).addClass("activeJoin");
+	// }
+
+	// if($(".glass").eq()==1){
+	// 	activee;
+	// }
+	
+
+});
