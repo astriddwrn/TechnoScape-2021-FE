9c3179c506aea62a687b2eb27e3edb912016fbb2
AsterinGray <raymondtiandinata@gmail.com>
2021-03-29 16:01:25 +0700

    FAQ Accordian complete

---
+++ js/pages/developer-workshop.js
@@ -0,0 +1,25 @@
+const expandQuestionsAnswer = (e) => {
+  let faqItem = e.closest('.faq-content-item');
+  let faqAns = $(faqItem.lastElementChild);
+  let activeItem = $('.faq-content-item.active');
+  let activeAns = $('.ans.active');
+
+  const removeActiveElm = () => {
+    $(activeItem).removeClass('active');
+    $(activeAns).slideToggle();
+    $(activeAns).removeClass('active');
+  };
+
+  const addActiveElm = () => {
+    $(faqItem).addClass('active');
+    $(faqAns).addClass('active');
+    $(faqAns).slideToggle();
+  };
+
+  if ($(faqItem).hasClass('active')) {
+    removeActiveElm();
+  } else {
+    removeActiveElm();
+    addActiveElm();
+  }
+};
