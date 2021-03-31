7e7596bd7b9cabd75328c0f04ef60c49d4289902
Alfian <alfian101@gmail.com>
2021-03-31 23:27:15 +0700

    faq responsive done

--- js/pages/hackathon.js
+++ js/pages/hackathon.js
@@ -23,3 +23,43 @@ $(document).ready(function () {
 		$(this).parent().find('img').toggle();
     });
 });
+
+var currPagenumb = 1;
+let currPagestring = "1 of 3";
+
+$(document).ready(function () {
+	$('#afterpage').click(function () {
+		if (currPagenumb == 1) {
+			currPagestring = "2 of 3";
+			$('#currpage').html(currPagestring);
+			currPagenumb = 2;
+			$("#faqPage1").addClass("faq-none");
+			$("#faqPage2").removeClass("faq-none");
+			$("#faqPage3").addClass("faq-none");
+		} else if (currPagenumb == 2) {
+			currPagestring = "3 of 3";
+			$('#currpage').html(currPagestring);
+			currPagenumb = 3;
+			$("#faqPage1").addClass("faq-none");
+			$("#faqPage2").addClass("faq-none");
+			$("#faqPage3").removeClass("faq-none");
+		}
+	});
+	$('#beforepage').click(function () {
+		if (currPagenumb == 2) {
+			currPagestring = "1 of 3";
+			$('#currpage').html(currPagestring);
+			currPagenumb = 1;
+			$("#faqPage1").removeClass("faq-none");
+			$("#faqPage2").addClass("faq-none");
+			$("#faqPage3").addClass("faq-none");
+		} else if (currPagenumb == 3) {
+			currPagestring = "2 of 3";
+			$('#currpage').html(currPagestring);
+			currPagenumb = 2;
+			$("#faqPage1").addClass("faq-none");
+			$("#faqPage2").removeClass("faq-none");
+			$("#faqPage3").addClass("faq-none");
+		}
+	});
+});
