3b52df5a4a1fd903f88470a3d7b4b0e024e58c85
Alfian <alfian101@gmail.com>
2021-04-07 15:34:37 +0700

    merged timeline in

--- js/pages/hackathon.js
+++ js/pages/hackathon.js
@@ -333,7 +333,34 @@ $(document).ready(function(){
     	useTransform: true,
     	cssEase: "ease-in-out"
 	});
+	
+		/* Time Trigger on Hackathon-Timeline */
+	var closeRegistration = new Date("June 20, 2021 23:59:59").getTime();
+	var technicalMeeting = new Date("June 24, 2021 23:59:59").getTime();
+	var competitionDay = new Date("June 26, 2021 23:59:59").getTime();
 
+	// Update the count down every 1 second
+	var timelineHackathonTrigger = setInterval(function () {
+		/* Get Today's date and time */
+		var now = new Date().getTime();
+		var triggerCloseregistration = closeRegistration - now;
+		var triggerTechnicalmeeting = technicalMeeting - now;
+		var triggercompetitionDay = competitionDay - now;
+
+		if (triggerCloseregistration < 0) {
+			$("#timeline-circle1").addClass("timeline-circle-active");
+			$("#timeline-vertical1").addClass("timeline-vertical-active");
+		}
+		if (triggerTechnicalmeeting < 0) {
+			$("#timeline-circle2").addClass("timeline-circle-active");
+			$("#timeline-vertical2").addClass("timeline-vertical-active");
+		}
+		if (triggercompetitionDay < 0) {
+			$("#timeline-circle3").addClass("timeline-circle-active");
+		}
+
+		/* Updates every 1 minutes, biar ga lag? */
+	}, 60000);
 
 	//   SHARE TO FRIEND
 	$(".shareToFriend .button-orange").click(function(){
