d4a41b08c591d8778031a870c0cedb016992cf12
Maryanto Cin <firevolt719@gmail.com>
2021-04-07 15:59:08 +0700

    faq done

--- js/pages/home.js
+++ js/pages/home.js
@@ -68,6 +68,38 @@ const validateEmail = (input) => {
   }
 };
 
+const expandQuestionsAnswer = (e) => {
+  let faqItem = e.closest(".faq-content-item");
+  let faqAns = $(faqItem.lastElementChild);
+  let activeItem = $(".faq-content-item.active");
+  let activeAns = $(".ans.active");
+
+  const removeActiveElm = () => {
+    $(activeItem).removeClass("active");
+    $(activeAns).slideToggle();
+    $(activeAns).removeClass("active");
+  };
+
+  const addActiveElm = () => {
+    $(faqItem).addClass("active");
+    $(faqAns).addClass("active");
+    $(faqAns).slideToggle();
+  };
+
+  if ($(faqItem).hasClass("active")) {
+    removeActiveElm();
+  } else {
+    removeActiveElm();
+    addActiveElm();
+  }
+};
+
+$(".faq-caro").slick({
+  prevArrow: $("#faq-nav-left"),
+  nextArrow: $("#faq-nav-right"),
+  dots: true,
+});
+
 $(".share .talk-btn").click(function () {
   $(".shareToFriend-overlay").css("display", "block");
 });
