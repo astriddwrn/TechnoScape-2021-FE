40b2edcb06e3e0ae527104840e287dbe504f9378
Alfian <alfian101@gmail.com>
2021-04-07 22:10:14 +0700

    merged faq

--- js/pages/hackathon.js
+++ js/pages/hackathon.js
@@ -334,6 +334,7 @@ $(document).ready(function(){
     	cssEase: "ease-in-out"
 	});
 	
+	/* Timeline */
 		/* Time Trigger on Hackathon-Timeline */
 	var closeRegistration = new Date("June 20, 2021 23:59:59").getTime();
 	var technicalMeeting = new Date("June 24, 2021 23:59:59").getTime();
@@ -361,6 +362,56 @@ $(document).ready(function(){
 
 		/* Updates every 1 minutes, biar ga lag? */
 	}, 60000);
+
+
+	/* FAQ */
+	$(document).ready(function () {
+		$('.question .question-top').click(function () {
+			$(this).next('.answer').slideToggle();
+			$(this).parent().find('img').toggle();
+		});
+	});
+	
+	var currPagenumb = 1;
+	let currPagestring = "1 of 3";
+
+	$(document).ready(function () {
+		$('#afterpage').click(function () {
+			if (currPagenumb == 1) {
+				currPagestring = "2 of 3";
+				$('#currpage').html(currPagestring);
+				currPagenumb = 2;
+				$("#faqPage1").addClass("faq-none");
+				$("#faqPage2").removeClass("faq-none");
+				$("#faqPage3").addClass("faq-none");
+			} else if (currPagenumb == 2) {
+				currPagestring = "3 of 3";
+				$('#currpage').html(currPagestring);
+				currPagenumb = 3;
+				$("#faqPage1").addClass("faq-none");
+				$("#faqPage2").addClass("faq-none");
+				$("#faqPage3").removeClass("faq-none");
+			}
+		});
+		$('#beforepage').click(function () {
+			if (currPagenumb == 2) {
+				currPagestring = "1 of 3";
+				$('#currpage').html(currPagestring);
+				currPagenumb = 1;
+				$("#faqPage1").removeClass("faq-none");
+				$("#faqPage2").addClass("faq-none");
+				$("#faqPage3").addClass("faq-none");
+			} else if (currPagenumb == 3) {
+				currPagestring = "2 of 3";
+				$('#currpage').html(currPagestring);
+				currPagenumb = 2;
+				$("#faqPage1").addClass("faq-none");
+				$("#faqPage2").removeClass("faq-none");
+				$("#faqPage3").addClass("faq-none");
+			}
+		});
+	});
+
 
 	//   SHARE TO FRIEND
 	$(".shareToFriend .button-orange").click(function(){
