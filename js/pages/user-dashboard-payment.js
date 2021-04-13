aec3f7bac7d78e82d2282eef59393a17267dead8
astriddwrn <astrid.dewi.rana@gmail.com>
2021-04-13 13:21:36 +0700

    user dashboard payment page hackathon responsive done

--- js/pages/user-dashboard-payment.js
+++ js/pages/user-dashboard-payment.js
@@ -4,5 +4,62 @@ $(document).ready(function() {
         var file = $('#receipt')[0].files[0].name;
         $(this).prev('label').text(file);
     });
-    // $('label').off('click');
+
+    $('input').change(function(){
+        if($(this).val()=='' && $(this).attr("type")!="radio"){
+            $(this).parent().removeClass('activeBorder');
+            $(".format").removeClass("errorRed");
+        }
+        if($(this).hasClass('error') && $(this).attr("type")!="radio"){
+            $(this).parent().addClass('errorBorder');
+            $(".format").addClass("errorRed");
+        }
+    });
+
+    $.validator.addMethod('filesize', function (value, element, param) {
+        return this.optional(element) || (element.files[0].size <= param)
+    });
+
+    jQuery(function ($) {
+        "use strict";
+        $("form").validate({
+            
+            rules: {
+                receipt: {
+                    required: true, 
+                    extension: "jpg,jpeg,png,pdf",
+                    filesize: 5242880
+                }
+            },
+            messages: {
+            },
+            errorPlacement: function(error, element) {
+                element.parent().addClass('errorBorder');
+                $(".format").addClass("errorRed");
+                    
+            },
+            unhighlight: function(element) {  
+                // element.parent().after().remove();
+                $(element).addClass("valid");
+                $(element).parent().removeClass('errorBorder');
+                $(".format").removeClass("errorRed");
+                $(element).parent().addClass('activeBorder');
+             }
+
+
+        });
+    });
+
+    $('.button-orange').click(function(){
+        $('form').valid();
+        var ariaInvalidNotFalse = $(".valid");
+        if(ariaInvalidNotFalse.length<1){
+            $('.button-orange').prop('disabled', 'disabled');
+        }
+        else{
+            $('.button-orange').prop('disabled', false);
+        }
+    });
+
+
 });
